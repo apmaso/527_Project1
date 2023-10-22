48babadeb902e8b53392e0d5ca0377806c291838
apmaso <amaso@pdx.edu>
2023-10-22 12:28:48 -0700

    w_matrix function should be completed now

--- parse_circuit_file.py
+++ parse_circuit_file.py
@@ -49,14 +49,27 @@ def create_wmatrix(circuit_info):
     
     Populate matrix with edge weights from circuit info
     """
-# Create a 2D array with n rows and columns
-size = circuit_info.get("total_nodes")
 
-# Populating matrices with zeros initially...
-# Will need to change to large value at some point=
-w_matrix = np.zeros((size,size))
+    # Create a 2D, square array with dimensions equal to the number of nodes
+    # Populate this matrix with a high number as initial_value
+    # This default value represents no path between the two nodes
+    dimension = circuit_info.get("total_nodes")
+    shape = (dimension, dimension)
+    fill_value = 999
+    w_matrix = np.full(shape,fill_value)
 
-# Access or modify elements using array[row, col]
+
+    # Modify elements in w-matrix using "edge_delays" dictionary in circuit_info
+    # Access using edge_name as key and edge_delay as the value. 
+    # edge_name ends in two integers: first=row, second=col 
+    for edge_name, edge_delay in circuit_info["edge_delays"].items():
+    
+        # First four characters are always the same, 'Edge'
+        i, j = map(int, edge_name[4:])
+        # Arrays start from 0. Row = (node-1) and Col = (node-1)
+        w_matrix[i-1, j-1] = edge_delay
+
+    return w_matrix
 
 
 
@@ -64,4 +77,6 @@ w_matrix = np.zeros((size,size))
 if __name__ == "__main__":
     file_path_txt = 'example_input2.txt'
     parsed_info = parse_circuit_file(file_path_txt)
+    w_matrix = create_wmatrix(parsed_info)
     print(parsed_info)
+    print(w_matrix)
